a606816dc3b8e846d6e4e2a4e0c95c8c380f951c
Brian Bassett <brianbassett@Brians-MBP.lan>
2022-02-10 20:11:08 -0500

    finished building the shows page and made it responsive for mobile/tablet. Still need to fix the footer and the gallery on the bio page

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,52 @@
+//<section class="main__shows">
+/* <h1 class="main__shows-header">Shows</h1>
+<div class="main__shows-labels">
+  <h6 class="main__shows-titles-tablet">DATE</h6>
+  <h6 class="main__shows-titles-tablet">VENUE</h6>
+  <h6 class="main__shows-titles-tablet">LOCATION</h6>
+  <div class="main__shows-empty"></div>
+</div> */
+
+/* <div class="main__shows-container">
+          <h6 class="main__shows-titles">DATE</h6>
+          <h5 class="main__shows-info main__shows-info--bold">
+            Mon Sept 06 2021
+          </h5>
+          <h6 class="main__shows-titles">VENUE</h6>
+          <p class="main__shows-info">Ronald Lane</p>
+          <h6 class="main__shows-titles">LOCATION</h6>
+          <p class="main__shows-info">San Francisco, CA</p>
+          <button class="main__shows-button">BUY TICKETS</button>
+        </div> */
+
+// const shows = [
+//     {
+//         date: "Mon Sept 06 2021",
+//         venue: "Ronald Lane",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Tues Sept 06 2021",
+//         venue: "BOBERT Lane",
+//         location: "NEW YORK, CA"
+//     }
+// ]
+
+// for (let i = 0; i<shows.length; i++) {
+//     const date = shows[i].date;
+//     const venue = shows[i].venue;
+//     const location = shows[i].location;
+
+//     const showCard = document.createElement("div"); //div for the card -append all the below to this
+//     const dateTitle = document.createElement("h6"); //these three are the labels
+//     const venueTitle = document.createElement("h6");
+//     const locationTitle = document.createElement("h6");
+//     const cardDate = document.createElement("h5"); //these three are the actual values
+//     const cardVenue = document.createElement("p")
+//     const cardLocation = document.createElement("p")
+
+// dateTitle.innerText = "DATE";
+// venueTitle.innerText = "VENUE";
+// locationTitle.innerText = "LOCATION";
+
+// }
