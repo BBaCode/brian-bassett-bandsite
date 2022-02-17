4ba7902a5247296e9bb3534326420fa00c7f746a
Brian Bassett <brianbassett@Brians-MBP.lan>
2022-02-17 09:42:25 -0500

    first commit for sprint 3, used axios to GET the info from comments and shows page. Fixed the timestamp to date conversion as well on the comments page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,108 +1,202 @@
-const shows = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Fransisco, CA",
-  },
-];
+const apiKey = "32d895a1-92c9-4310-bb26-8e03ec7bf488";
+const showsURL = "https://project-1-api.herokuapp.com/showdates?api_key=";
+const getShows = showsURL + apiKey;
 
-const showsSection = document.querySelector(".main__shows-desktop");
+const shows = [];
 
-const tabletCard = document.createElement("ul");
-const tabletDate = document.createElement("ul");
-const tabletVenue = document.createElement("ul");
-const tabletLocation = document.createElement("ul");
-const tabletDiv = document.createElement("div");
+//put axios in
 
-tabletDate.innerText = "DATE";
-tabletVenue.innerText = "VENUE";
-tabletLocation.innerText = "LOCATION";
+axios
+  .get(getShows)
+  .then((response) => {
+    for (i = 0; i < response.data.length; i++) {
+      shows[i] = response.data[i];
+    }
+    const showsSection = document.querySelector(".main__shows-desktop");
 
-tabletCard.classList.add("main__shows-labels");
-tabletDate.classList.add("main__shows-titles-tablet");
-tabletVenue.classList.add("main__shows-titles-tablet");
-tabletLocation.classList.add("main__shows-titles-tablet");
-tabletDiv.classList.add("main__shows-empty");
+    const tabletCard = document.createElement("ul");
+    const tabletDate = document.createElement("ul");
+    const tabletVenue = document.createElement("ul");
+    const tabletLocation = document.createElement("ul");
+    const tabletDiv = document.createElement("div");
 
-tabletCard.appendChild(tabletDate);
-tabletCard.appendChild(tabletVenue);
-tabletCard.appendChild(tabletLocation);
-tabletCard.appendChild(tabletDiv);
+    tabletDate.innerText = "DATE";
+    tabletVenue.innerText = "VENUE";
+    tabletLocation.innerText = "LOCATION";
 
-showsSection.appendChild(tabletCard);
+    tabletCard.classList.add("main__shows-labels");
+    tabletDate.classList.add("main__shows-titles-tablet");
+    tabletVenue.classList.add("main__shows-titles-tablet");
+    tabletLocation.classList.add("main__shows-titles-tablet");
+    tabletDiv.classList.add("main__shows-empty");
 
-for (let i = 0; i < shows.length; i++) {
-  const date = shows[i].date;
-  const venue = shows[i].venue;
-  const location = shows[i].location;
+    tabletCard.appendChild(tabletDate);
+    tabletCard.appendChild(tabletVenue);
+    tabletCard.appendChild(tabletLocation);
+    tabletCard.appendChild(tabletDiv);
 
-  //creating all of the elements below
+    showsSection.appendChild(tabletCard);
 
-  const showCard = document.createElement("ul"); //div for the card -append all the below to this
-  const dateTitle = document.createElement("li"); //these three are the labels
-  const venueTitle = document.createElement("li");
-  const locationTitle = document.createElement("li");
-  const cardDate = document.createElement("li"); //these three are the actual values
-  const cardVenue = document.createElement("li");
-  const cardLocation = document.createElement("li");
-  const cardButton = document.createElement("button");
+    for (let i = 0; i < shows.length; i++) {
+      const date = shows[i].date;
+      const venue = shows[i].venue;
+      const location = shows[i].location;
 
-  //giving inner Text to everything
+      //creating all of the elements below
 
-  dateTitle.innerText = "DATE";
-  venueTitle.innerText = "VENUE";
-  locationTitle.innerText = "LOCATION";
-  cardDate.innerText = date;
-  cardVenue.innerText = venue;
-  cardLocation.innerText = location;
-  cardButton.innerText = "BUY TICKETS";
+      const showCard = document.createElement("ul"); //div for the card -append all the below to this
+      const dateTitle = document.createElement("li"); //these three are the labels
+      const venueTitle = document.createElement("li");
+      const locationTitle = document.createElement("li");
+      const cardDate = document.createElement("li"); //these three are the actual values
+      const cardVenue = document.createElement("li");
+      const cardLocation = document.createElement("li");
+      const cardButton = document.createElement("button");
 
-  //adding classes to all of the elements
+      //giving inner Text to everything
 
-  showCard.classList.add("main__shows-container");
-  dateTitle.classList.add("main__shows-titles");
-  venueTitle.classList.add("main__shows-titles");
-  locationTitle.classList.add("main__shows-titles");
-  cardDate.classList.add("main__shows-info");
-  cardDate.classList.add("main__shows-info-bold");
-  cardVenue.classList.add("main__shows-info");
-  cardLocation.classList.add("main__shows-info");
-  cardButton.classList.add("main__shows-button");
+      dateTitle.innerText = "DATE";
+      venueTitle.innerText = "VENUE";
+      locationTitle.innerText = "LOCATION";
+      cardDate.innerText = date;
+      cardVenue.innerText = venue;
+      cardLocation.innerText = location;
+      cardButton.innerText = "BUY TICKETS";
 
-  //appending all of the elements
+      //adding classes to all of the elements
 
-  showCard.appendChild(dateTitle);
-  showCard.appendChild(cardDate);
-  showCard.appendChild(venueTitle);
-  showCard.appendChild(cardVenue);
-  showCard.appendChild(locationTitle);
-  showCard.appendChild(cardLocation);
-  showCard.appendChild(cardButton);
+      showCard.classList.add("main__shows-container");
+      dateTitle.classList.add("main__shows-titles");
+      venueTitle.classList.add("main__shows-titles");
+      locationTitle.classList.add("main__shows-titles");
+      cardDate.classList.add("main__shows-info");
+      cardDate.classList.add("main__shows-info-bold");
+      cardVenue.classList.add("main__shows-info");
+      cardLocation.classList.add("main__shows-info");
+      cardButton.classList.add("main__shows-button");
 
-  showsSection.appendChild(showCard);
-}
+      //appending all of the elements
+
+      showCard.appendChild(dateTitle);
+      showCard.appendChild(cardDate);
+      showCard.appendChild(venueTitle);
+      showCard.appendChild(cardVenue);
+      showCard.appendChild(locationTitle);
+      showCard.appendChild(cardLocation);
+      showCard.appendChild(cardButton);
+
+      showsSection.appendChild(showCard);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// const shows = [
+//   {
+//     date: "Mon Sept 06 2021",
+//     venue: "Ronald Lane",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Tue Sept 21 2021",
+//     venue: "Pier 3 East",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Fri Oct 15 2021",
+//     venue: "View Lounge",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Sat Nov 06 2021",
+//     venue: "Hyatt Agency",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Fri Nov 26 2021",
+//     venue: "Moscow Center",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: "Wed Dec 15 2021",
+//     venue: "Press Club",
+//     location: "San Fransisco, CA",
+//   },
+// ];
+
+// const showsSection = document.querySelector(".main__shows-desktop");
+
+// const tabletCard = document.createElement("ul");
+// const tabletDate = document.createElement("ul");
+// const tabletVenue = document.createElement("ul");
+// const tabletLocation = document.createElement("ul");
+// const tabletDiv = document.createElement("div");
+
+// tabletDate.innerText = "DATE";
+// tabletVenue.innerText = "VENUE";
+// tabletLocation.innerText = "LOCATION";
+
+// tabletCard.classList.add("main__shows-labels");
+// tabletDate.classList.add("main__shows-titles-tablet");
+// tabletVenue.classList.add("main__shows-titles-tablet");
+// tabletLocation.classList.add("main__shows-titles-tablet");
+// tabletDiv.classList.add("main__shows-empty");
+
+// tabletCard.appendChild(tabletDate);
+// tabletCard.appendChild(tabletVenue);
+// tabletCard.appendChild(tabletLocation);
+// tabletCard.appendChild(tabletDiv);
+
+// showsSection.appendChild(tabletCard);
+
+// for (let i = 0; i < shows.length; i++) {
+//   const date = shows[i].date;
+//   const venue = shows[i].venue;
+//   const location = shows[i].location;
+
+//   //creating all of the elements below
+
+//   const showCard = document.createElement("ul"); //div for the card -append all the below to this
+//   const dateTitle = document.createElement("li"); //these three are the labels
+//   const venueTitle = document.createElement("li");
+//   const locationTitle = document.createElement("li");
+//   const cardDate = document.createElement("li"); //these three are the actual values
+//   const cardVenue = document.createElement("li");
+//   const cardLocation = document.createElement("li");
+//   const cardButton = document.createElement("button");
+
+//   //giving inner Text to everything
+
+//   dateTitle.innerText = "DATE";
+//   venueTitle.innerText = "VENUE";
+//   locationTitle.innerText = "LOCATION";
+//   cardDate.innerText = date;
+//   cardVenue.innerText = venue;
+//   cardLocation.innerText = location;
+//   cardButton.innerText = "BUY TICKETS";
+
+//   //adding classes to all of the elements
+
+//   showCard.classList.add("main__shows-container");
+//   dateTitle.classList.add("main__shows-titles");
+//   venueTitle.classList.add("main__shows-titles");
+//   locationTitle.classList.add("main__shows-titles");
+//   cardDate.classList.add("main__shows-info");
+//   cardDate.classList.add("main__shows-info-bold");
+//   cardVenue.classList.add("main__shows-info");
+//   cardLocation.classList.add("main__shows-info");
+//   cardButton.classList.add("main__shows-button");
+
+//   //appending all of the elements
+
+//   showCard.appendChild(dateTitle);
+//   showCard.appendChild(cardDate);
+//   showCard.appendChild(venueTitle);
+//   showCard.appendChild(cardVenue);
+//   showCard.appendChild(locationTitle);
+//   showCard.appendChild(cardLocation);
+//   showCard.appendChild(cardButton);
+
+//   showsSection.appendChild(showCard);
+// }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,106 +1,217 @@
-const defaultComments = [
-  {
-    name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+const apiKey = "32d895a1-92c9-4310-bb26-8e03ec7bf488";
+const commentURL = "https://project-1-api.herokuapp.com/comments?api_key=";
+const getComments = commentURL + apiKey;
 
-console.log(defaultComments);
+const defaultComments = [];
 
-const commentSection = document.getElementById("commentCard");
-const formComments = document.getElementById("form");
+axios
+  .get(getComments)
+  .then((response) => {
+    for (i = 0; i < response.data.length; i++) {
+      defaultComments[i] = response.data[i];
+    }
+    const commentSection = document.getElementById("commentCard");
+    const formComments = document.getElementById("form");
 
-function displayComment(arr) {
-  let name = arr.name;
-  let date = arr.date;
-  let comment = arr.comment;
+    // make this work
+    // const resetField = document.querySelectorAll(".main__comments-field");
+    // resetField.value = "";
 
-  //creating all of the elements
+    function displayComment(arr) {
+      const name = arr.name;
+      const timestamp = arr.timestamp;
+      const timeItem = new Date(timestamp);
+      const date =
+        timeItem.getMonth() +
+        1 +
+        "/" +
+        timeItem.getDate() +
+        "/" +
+        timeItem.getFullYear();
+      const comment = arr.comment;
 
-  let commentCard = document.createElement("div");
-  //the commentCard holds all of the below
-  let avatarCard = document.createElement("img");
-  let rightCard = document.createElement("div");
-  let nameDateCardBox = document.createElement("div");
-  //the nameDateCardBox holds the nameCard and dateCard
-  let nameCard = document.createElement("h5");
-  let dateCard = document.createElement("p");
-  //card bottom
-  let paragraphCard = document.createElement("p");
+      //creating all of the elements
 
-  //putting text/content/attributes in all of the elements
+      let commentCard = document.createElement("div");
+      //the commentCard holds all of the below
+      let avatarCard = document.createElement("img");
+      let rightCard = document.createElement("div");
+      let nameDateCardBox = document.createElement("div");
+      //the nameDateCardBox holds the nameCard and dateCard
+      let nameCard = document.createElement("h5");
+      let dateCard = document.createElement("p");
+      //card bottom
+      let paragraphCard = document.createElement("p");
 
-  avatarCard.setAttribute("src", "assets/images/mercury.png");
-  nameCard.innerText = name;
-  dateCard.innerText = date;
-  paragraphCard.innerText = comment;
+      //putting text/content/attributes in all of the elements
 
-  //putting classes on all of the elements
+      avatarCard.setAttribute("src", "assets/images/mercury.png");
+      nameCard.innerText = name;
+      dateCard.innerText = date;
+      paragraphCard.innerText = comment;
 
-  commentCard.classList.add("main__comments-card");
-  avatarCard.classList.add("main__comments-empty-avatar");
-  rightCard.classList.add("main__comments-card__right");
-  nameDateCardBox.classList.add("main__comments-card__right-top");
-  nameCard.classList.add("main__comments-card__right-top-header");
-  dateCard.classList.add("main__comments-card__right-top-date");
-  paragraphCard.classList.add("main__comments-card__bottom-paragraph");
+      //putting classes on all of the elements
 
-  //append everything together here
+      commentCard.classList.add("main__comments-card");
+      avatarCard.classList.add("main__comments-empty-avatar");
+      rightCard.classList.add("main__comments-card__right");
+      nameDateCardBox.classList.add("main__comments-card__right-top");
+      nameCard.classList.add("main__comments-card__right-top-header");
+      dateCard.classList.add("main__comments-card__right-top-date");
+      paragraphCard.classList.add("main__comments-card__bottom-paragraph");
 
-  commentSection.appendChild(commentCard);
-  commentCard.appendChild(avatarCard);
-  commentCard.appendChild(rightCard);
-  rightCard.appendChild(nameDateCardBox);
-  nameDateCardBox.appendChild(nameCard);
-  nameDateCardBox.appendChild(dateCard);
-  rightCard.appendChild(paragraphCard);
-}
+      //append everything together here
 
-defaultComments.forEach((comment) => {
-  displayComment(comment);
-});
-
-//THIS ADDS TO THE FRONT OF THE ARRAY BUT PRINTS UNDEFINED AT THE BOTTOM
-//TRIED TO FOLLOW NIGELS CODE BUT THE DEMO DID NOT WORK FOR ADDING ELEMENTS FROM FORM INPUTS UPON OPENING, COULDNT REMEMBER WHAT HE CHANGED
-
-formComments.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let nameItem = event.target.name.value;
-  let dateItem = new Date();
-  let dateOfComment =
-    dateItem.getMonth() +
-    1 +
-    "/" +
-    dateItem.getDate() +
-    "/" +
-    dateItem.getFullYear();
-  let commentItem = event.target.comment.value;
-
-  if (nameItem && dateItem && commentItem) {
-    defaultComments.unshift({
-      name: nameItem,
-      date: dateOfComment,
-      comment: commentItem,
+      commentSection.appendChild(commentCard);
+      commentCard.appendChild(avatarCard);
+      commentCard.appendChild(rightCard);
+      rightCard.appendChild(nameDateCardBox);
+      nameDateCardBox.appendChild(nameCard);
+      nameDateCardBox.appendChild(dateCard);
+      rightCard.appendChild(paragraphCard);
+    }
+    defaultComments.forEach((comment) => {
+      displayComment(comment);
     });
-    displayComment(defaultComments);
-  }
-  let resetName = document.querySelectorAll(".main__comments-field");
-  if (!nameItem) {
-    resetName.classList.add("main__comment--error");
-  }
-});
+
+    formComments.addEventListener("submit", (event) => {
+      event.preventDefault();
+      commentSection.innerText = "";
+      let nameItem = event.target.name.value;
+      let dateOfComment = Date.now();
+      let commentItem = event.target.comment.value;
+
+      if (nameItem && dateOfComment && commentItem) {
+        defaultComments.unshift({
+          name: nameItem,
+          timestamp: dateOfComment,
+          comment: commentItem,
+        });
+        console.log(defaultComments);
+        defaultComments.forEach((comment) => {
+          displayComment(comment);
+        });
+      }
+      let resetName = document.querySelectorAll(".main__comments-field");
+      if (!nameItem) {
+        resetName.classList.add("main__comment--error");
+      }
+    });
+    console.log(defaultComments);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// const defaultComments = [
+//   {
+//     name: "Connor Walton",
+//     date: "02/17/2021",
+//     comment:
+//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   },
+//   {
+//     name: "Emilie Beach",
+//     date: "01/09/2021",
+//     comment:
+//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//   },
+//   {
+//     name: "Miles Acosta",
+//     date: "12/20/2020",
+//     comment:
+//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//   },
+// ];
+
+// console.log(defaultComments);
+
+// const commentSection = document.getElementById("commentCard");
+// const formComments = document.getElementById("form");
+
+// // make this work
+// // const resetField = document.querySelectorAll(".main__comments-field");
+// // resetField.innerHTML = " ";
+
+// function displayComment(arr) {
+//   let name = arr.name;
+//   let date = arr.date;
+//   let comment = arr.comment;
+
+//   //creating all of the elements
+
+//   let commentCard = document.createElement("div");
+//   //the commentCard holds all of the below
+//   let avatarCard = document.createElement("img");
+//   let rightCard = document.createElement("div");
+//   let nameDateCardBox = document.createElement("div");
+//   //the nameDateCardBox holds the nameCard and dateCard
+//   let nameCard = document.createElement("h5");
+//   let dateCard = document.createElement("p");
+//   //card bottom
+//   let paragraphCard = document.createElement("p");
+
+//   //putting text/content/attributes in all of the elements
+
+//   avatarCard.setAttribute("src", "assets/images/mercury.png");
+//   nameCard.innerText = name;
+//   dateCard.innerText = date;
+//   paragraphCard.innerText = comment;
+
+//   //putting classes on all of the elements
+
+//   commentCard.classList.add("main__comments-card");
+//   avatarCard.classList.add("main__comments-empty-avatar");
+//   rightCard.classList.add("main__comments-card__right");
+//   nameDateCardBox.classList.add("main__comments-card__right-top");
+//   nameCard.classList.add("main__comments-card__right-top-header");
+//   dateCard.classList.add("main__comments-card__right-top-date");
+//   paragraphCard.classList.add("main__comments-card__bottom-paragraph");
+
+//   //append everything together here
+
+//   commentSection.appendChild(commentCard);
+//   commentCard.appendChild(avatarCard);
+//   commentCard.appendChild(rightCard);
+//   rightCard.appendChild(nameDateCardBox);
+//   nameDateCardBox.appendChild(nameCard);
+//   nameDateCardBox.appendChild(dateCard);
+//   rightCard.appendChild(paragraphCard);
+// }
+// defaultComments.forEach((comment) => {
+//   displayComment(comment);
+// });
+
+// //THIS ADDS TO THE FRONT OF THE ARRAY BUT PRINTS UNDEFINED AT THE BOTTOM
+// //TRIED TO FOLLOW NIGELS CODE BUT THE DEMO DID NOT WORK FOR ADDING ELEMENTS FROM FORM INPUTS UPON OPENING, COULDNT REMEMBER WHAT HE CHANGED
+
+// formComments.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   commentSection.innerText = "";
+//   let nameItem = event.target.name.value;
+//   let dateItem = new Date();
+//   let dateOfComment =
+//     dateItem.getMonth() +
+//     1 +
+//     "/" +
+//     dateItem.getDate() +
+//     "/" +
+//     dateItem.getFullYear();
+//   let commentItem = event.target.comment.value;
+
+//   if (nameItem && dateItem && commentItem) {
+//     defaultComments.unshift({
+//       name: nameItem,
+//       date: dateOfComment,
+//       comment: commentItem,
+//     });
+//     console.log(defaultComments);
+//     defaultComments.forEach((comment) => {
+//       displayComment(comment);
+//     });
+//   }
+//   let resetName = document.querySelectorAll(".main__comments-field");
+//   if (!nameItem) {
+//     resetName.classList.add("main__comment--error");
+//   }
+// });
